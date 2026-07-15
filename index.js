524ba2463c8ba5648c9af6f301ed7198e6148c03
Hemachandru <2006hemachandru@gmail.com>
2026-07-15 10:11:56 +0530

    Fix TODO comments and import createRoot

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Create a React element using React.createElement()
 // Tag name: h1
 // Text: Welcome to React
-import {createElement} from 'react-dom/client';
+import {createElement, createRoot} from 'react-dom/client';
 
 const domRoot = React.createElement("div", {id: "root"}, createElement("h1", null, "Welome to React Framework"));                                       
 const rootElement = document.getElementById("root");
